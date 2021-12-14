011bd1f3c2c3ce2fa00decedfcce9078e4d9ff06
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2021-12-14 19:55:35 +0530

    Moved to SVG rendering

--- src/Node.tsx
+++ src/Node.tsx
@@ -5,91 +5,35 @@ export type Vec2 = {
     x: number,
     y: number,
 }
-export interface NodeProps extends React.HTMLAttributes<HTMLDivElement> {
-    name: string
-    node_id: number
-    pos: Vec2
-    inputs: number
-    outputs: number
+
+export type ConnectorType = "input" | "output"
+
+export interface NodeProps extends React.SVGAttributes<SVGGElement> {
+    node_id: number,
+    pos: Vec2,
+    size: Vec2,
 }
 
+export function calculateNodeSize(max_conn: number, content_size: Vec2 = { x: 50, y: 50 }): Vec2 {
+    return {
+        x: content_size.x + 40,
+        y: Math.max(content_size.y, max_conn * 20 + 5),
+    }
+}
 
 export class Node extends React.Component<NodeProps, {}> {
     public static defaultProps = {
-        name: "UNNAMED",
         pos: { x: 0, y: 0 },
-        inputs: 0,
-        outputs: 0,
-    }
-    ref: HTMLDivElement | undefined
-    inputs: Vec2[]
-    outputs: Vec2[]
-
-    constructor(props: NodeProps) {
-        super(props);
-        this.inputs = Array(props.inputs).map(_ => ({ x: 0, y: 0 }))
-        this.outputs = Array(props.outputs).map(_ => ({ x: 0, y: 0 }))
+        size: { x: 150, y: 75 },
     }
 
     render() {
-        const { name, pos, ...others } = this.props;
+        const { node_id, pos, size } = this.props;
         return (
-            <div
-                ref={r => { if (r) this.ref = r }}
-                style={{
-                    left: pos.x + 'px',
-                    top: pos.y + 'px',
-                    height: 20 + 20 * Math.max(this.props.inputs, this.props.outputs),
-                }}
-                className='Node'
-                {...others}
-            >
-                <div className='ConnectorContainer'>
-                    {Array(this.props.inputs).fill(0).map((_, i) =>
-                        <Connector
-                            ref={r => r && r.ref && (
-                                this.inputs[i] = { x: r.ref.offsetLeft, y: r.ref.offsetTop }
-                            )}
-                            key={i}
-                            id={i}
-                            type="in"
-                            parent_id={this.props.node_id}
-                        />
-                    )}
-                </div>
-                {this.props.name}
-                <div className='ConnectorContainer'>
-                    {Array(this.props.outputs).fill(0).map((_, i) =>
-                        <Connector
-                            ref={r => r && r.ref && (
-                                this.outputs[i] = { x: r.ref.offsetLeft, y: r.ref.offsetTop }
-                            )}
-                            key={i}
-                            id={i}
-                            type="out"
-                            parent_id={this.props.node_id}
-                        />
-                    )}
-                </div>
-            </div>
-        );
-    }
-}
-
-interface ConnectorProps {
-    id: number,
-    type: "in" | "out",
-    parent_id: number,
-}
-
-class Connector extends React.Component<ConnectorProps> {
-    ref: HTMLDivElement | undefined
-
-    render(): React.ReactNode {
-        return (
-            <div ref={r => { if (r) this.ref = r }} className={'Connector' + this.props.type}>
-                {this.props.id}
-            </div>
+            <g id={"node_" + node_id} {...(this.props as React.SVGAttributes<SVGGElement>)}>
+                <rect width={size.x} height={size.y} x={pos.x} y={pos.y} className="Node" fill="red" />
+                {this.props.children}
+            </g>
         );
     }
 }
--- src/Stage.tsx
+++ src/Stage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Node, NodeProps, Vec2 } from './Node'
+import { Node, NodeProps, Vec2, calculateNodeSize, ConnectorType } from './Node'
 import "./Stage.scss"
 
 interface StageProps {
@@ -7,96 +7,129 @@ interface StageProps {
     height: number,
 }
 
+type Edge = {
+    from_type: ConnectorType,
+    from_conn: number,
+    to: number,
+    to_type: ConnectorType,
+    to_conn: number,
+}
+
 interface NodeData extends NodeProps {
-    node_id: number,
-    isdragging: boolean,
-    rel: Vec2,
-    dims: Vec2,
-    ref: Node | undefined,
-    mousemove: (e: MouseEvent) => void,
-    mouseup: (e: MouseEvent) => void,
+    inputs: number,
+    outputs: number,
+    edges: Edge[]
 }
 
 interface StageState {
-    nodes: NodeData[]
-    graph: Record<number, number[]>
+    nodes: NodeData[],
+    isdragging: boolean,
+    dragging_ind: number,
+    rel: Vec2,
+}
+
+const DefaultNodeData = {
+    pos: { x: 0, y: 0 },
+    size: { x: 150, y: 75 },
+    inputs: 1,
+    outputs: 1,
+    edges: [],
+};
+
+function calculateConnectorX(parent: NodeData, type: ConnectorType) {
+    if (type === "input")
+        return parent.pos.x + 12.5;
+    else
+        return parent.pos.x + parent.size.x - 12.5;
+}
+
+function calculateConnectorY(parent: NodeData, index: number) {
+    return parent.pos.y + (index - 1) * 20 + 12.5;
+}
+
+function calculateContentX(parent: NodeData) {
+    return parent.pos.x + 20;
+}
+
+function calculateContentY(parent: NodeData) {
+    return parent.pos.y + parent.size.y / 2;
 }
 
 export class Stage extends React.Component<StageProps, StageState> {
     state: StageState = {
         nodes: [
             {
+                ...DefaultNodeData,
                 node_id: 0,
-                name: "First",
-                pos: { x: 0, y: 0 },
-                isdragging: false,
-                rel: { x: 0, y: 0 },
-                dims: { x: 0, y: 0 },
-                inputs: 1,
-                outputs: 2,
-                mousemove: e => this.onNodeMouseMove(0, e),
-                mouseup: e => this.onNodeMouseUp(0, e),
+                edges: [{
+                    from_type: "output",
+                    from_conn: 1,
+                    to: 1,
+                    to_type: "input",
+                    to_conn: 1,
+                }]
             } as NodeData,
             {
+                ...DefaultNodeData,
                 node_id: 1,
-                name: "Second",
                 pos: { x: 50, y: 50 },
-                isdragging: false,
-                rel: { x: 0, y: 0 },
-                dims: { x: 0, y: 0 },
                 inputs: 2,
                 outputs: 1,
-                mousemove: e => this.onNodeMouseMove(1, e),
-                mouseup: e => this.onNodeMouseUp(1, e),
             } as NodeData
         ],
-        graph: { 0: [], 1: [] }
+        isdragging: false,
+        dragging_ind: -1,
+        rel: { x: 0, y: 0 },
     }
 
     constructor(props: StageProps) {
         super(props);
+
+        this.setState({
+            nodes: this.state.nodes.map(node => ({
+                ...node,
+                size: calculateNodeSize(Math.max(node.inputs, node.outputs))
+            }))
+        })
         this.onNodeMouseDown = this.onNodeMouseDown.bind(this);
+        this.onNodeMouseUp = this.onNodeMouseUp.bind(this);
+        this.onNodeMouseMove = this.onNodeMouseMove.bind(this);
     }
 
     componentDidUpdate(_: StageProps, prestate: StageState) {
-        for (let ind = 0; ind < this.state.nodes.length; ind++) {
-            if (this.state.nodes[ind].isdragging && !prestate.nodes[ind].isdragging) {
-                document.addEventListener('mousemove', this.state.nodes[ind].mousemove);
-                document.addEventListener('mouseup', this.state.nodes[ind].mouseup);
-            }
-            else if (!this.state.nodes[ind].isdragging && prestate.nodes[ind].isdragging) {
-                document.removeEventListener('mousemove', this.state.nodes[ind].mousemove);
-                document.removeEventListener('mouseup', this.state.nodes[ind].mouseup);
-            }
+        if (this.state.isdragging && !prestate.isdragging) {
+            document.addEventListener('mousemove', this.onNodeMouseMove);
+            document.addEventListener('mouseup', this.onNodeMouseUp);
+        }
+        else if (!this.state.isdragging && prestate.isdragging) {
+            document.removeEventListener('mousemove', this.onNodeMouseMove);
+            document.removeEventListener('mouseup', this.onNodeMouseUp);
         }
     }
 
-    onNodeMouseUp(id: number, e: MouseEvent): void {
-        const ind = this.state.nodes.findIndex(o => o.node_id === id);
-        let nodes = this.state.nodes;
-        nodes[ind].isdragging = false;
-        this.setState({ nodes: nodes });
+    onNodeMouseUp(e: MouseEvent): void {
+        this.setState({ isdragging: false, dragging_ind: -1 });
         e.stopPropagation();
         e.preventDefault();
     }
 
-    onNodeMouseMove(id: number, e: MouseEvent): void {
+    onNodeMouseMove(e: MouseEvent): void {
         this.setState({
             nodes: this.state.nodes.map(node => {
-                if (node.node_id !== id || !node.isdragging)
+                if (node.node_id !== this.state.dragging_ind)
                     return node;
                 let newpos = {
-                    x: e.pageX - node.rel.x,
-                    y: e.pageY - node.rel.y,
+                    x: e.pageX - this.state.rel.x,
+                    y: e.pageY - this.state.rel.y,
                 }
-                if (node.dims.x >= this.props.width || node.dims.y >= this.props.height)
+                if (node.size.x >= this.props.width || node.size.y >= this.props.height)
                     return node;
                 if (newpos.x < 0) newpos.x = 0;
-                if (newpos.x > this.props.width - node.dims.x)
-                    newpos.x = this.props.width - node.dims.x;
+                if (newpos.x > this.props.width - node.size.x)
+                    newpos.x = this.props.width - node.size.x;
                 if (newpos.y < 0) newpos.y = 0;
-                if (newpos.y > this.props.height - node.dims.y)
-                    newpos.y = this.props.height - node.dims.y;
+                if (newpos.y > this.props.height - node.size.y)
+                    newpos.y = this.props.height - node.size.y;
                 return {
                     ...node,
                     pos: newpos,
@@ -111,53 +144,88 @@ export class Stage extends React.Component<StageProps, StageState> {
     onNodeMouseDown(id: number, e: React.MouseEvent) {
         if (e.button !== 0)
             return;
+        const ind = this.state.nodes.findIndex(n => n.node_id === id);
+        const node = this.state.nodes[ind];
         this.setState({
-            nodes: this.state.nodes.map(
-                node => {
-                    if (node.node_id === id) {
-                        return {
-                            ...node,
-                            isdragging: true,
-                            rel: {
-                                x: e.pageX - node.pos.x,
-                                y: e.pageY - node.pos.y,
-                            }
-                        }
-                    }
-                    return node;
-                }
-            )
+            isdragging: true,
+            dragging_ind: ind,
+            rel: {
+                x: e.pageX - node.pos.x,
+                y: e.pageY - node.pos.y,
+            }
         });
         e.stopPropagation();
         e.preventDefault();
     }
 
-    render(): React.ReactNode {
+    render() {
         let foo = (
-            <div
+            <svg
                 className="Stage"
-                style={{ width: this.props.width + "px", height: this.props.height + "px" }}
+                width={this.props.width}
+                height={this.props.height}
             >
                 {this.state.nodes.map(node => (
                     <Node
                         key={node.node_id}
-                        ref={r => {
-                            if (r && r.ref) {
-                                node.dims = { x: r.ref.offsetWidth, y: r.ref.offsetHeight }
-                                node.ref = r;
-                            }
-                        }}
-                        name={node.name}
-                        pos={node.pos}
-                        node_id={node.node_id}
-                        inputs={node.inputs}
-                        outputs={node.outputs}
+                        {...(node as NodeProps)}
                         onMouseDown={(e: React.MouseEvent) =>
                             this.onNodeMouseDown(node.node_id, e)
                         }
-                    />
+                    >
+                        <text
+                            x={calculateContentX(node)}
+                            y={calculateContentY(node)}
+                            className="ContentText"
+                        >
+                            TEXT
+                        </text>
+                        {Array(node.inputs).fill(1).map((_, i) =>
+                            <circle
+                                className="Connector"
+                                key={i}
+                                cx={calculateConnectorX(node, "input")}
+                                cy={calculateConnectorY(node, i + 1)}
+                            />
+                        )}
+                        {Array(node.outputs).fill(1).map((_, i) =>
+                            <circle
+                                key={i}
+                                className="Connector"
+                                cx={calculateConnectorX(node, "output")}
+                                cy={calculateConnectorY(node, i + 1)}
+                            />
+                        )}
+                    </Node>
                 ))}
-            </div>
+                {this.state.nodes.map(node =>
+                    node.edges.map(edge => {
+                        if (node.node_id < edge.to)
+                            return <line
+                                key={
+                                    `${node.node_id}_${edge.from_type}` +
+                                    `${edge.from_conn}_${edge.to}_${edge.to_type}${edge.to_conn}`
+                                }
+                                x1={calculateConnectorX(node, edge.from_type)}
+                                y1={calculateConnectorY(node, edge.from_conn)}
+                                x2={calculateConnectorX(
+                                    this.state.nodes[
+                                    this.state.nodes.findIndex(n => n.node_id === edge.to)
+                                    ],
+                                    edge.to_type
+                                )}
+                                y2={calculateConnectorY(
+                                    this.state.nodes[
+                                    this.state.nodes.findIndex(n => n.node_id === edge.to)
+                                    ],
+                                    edge.to_conn)}
+                                stroke="rgb(0,255,0)"
+                                strokeWidth={2}
+                            />;
+                        return <g></g>
+                    })
+                )}
+            </svg>
         );
 
         return foo;
